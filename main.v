db19330bac7c47fa2618a339f04df429fc939260
Charles <charles.rosenbauer@gmail.com>
2024-11-20 21:29:19 -0600

    Sketching out solver a bit

--- main.v
+++ main.v
@@ -1,16 +1,23 @@
 /*
 	Okay, actually I'm going to build something more interesting.
 	I want to build a verilog SAT solver
+	
+	There is a command buffer, and some input/output buffers.
+	Also outputs for sat and unsat, to signal completion.
+	
+	There are some commands for reseting state, initializing clauses, and evaluating the problem, and extracting the final state.
 */
 
-
-
-module myModule();
-
-initial
-  begin
-    $display("Hello World!");
-    $finish ;
-  end
+module solver(
+	input wire			clk,
+	input wire [7:0]	bus_a,
+	input wire [7:0]	bus_b,
+	input wire [7:0]	bus_c,
+	input wire [7:0]	cmd,
+	
+	output reg			sat,
+	output reg			unsat,
+	output reg [7:0]	exbus
+);
 
 endmodule
