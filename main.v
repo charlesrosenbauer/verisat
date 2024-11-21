4304413487071fc6a406b34acbfb93435b264fe9
Charles <charles.rosenbauer@gmail.com>
2024-11-20 19:47:26 -0600

    Changing this to a verilog sat solver project, that seems a lot more interesting than a blitter

---
+++ main.v
@@ -0,0 +1,16 @@
+/*
+	Okay, actually I'm going to build something more interesting.
+	I want to build a verilog SAT solver
+*/
+
+
+
+module myModule();
+
+initial
+  begin
+    $display("Hello World!");
+    $finish ;
+  end
+
+endmodule
